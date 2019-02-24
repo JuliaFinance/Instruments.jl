89217bdf6e52cac39ea28159f5638d6810636c3f
Eric Forgy <Eric.Forgy@coherentasia.com>
2019-02-24 23:18:12 +0800

    Implement Positions.jl
    Remove Union with Currency
    
    Update deps
    
    Update FI -> v0.1.1
    
    ::Float64 -> A<:Real
    
    Float64 -> <:Real
    
    Add FixedPointDecimals
    
    Add promotion rules
    
    Add zero and one
    
    Minor clean up
    
    Fix promotion rules
    
    CA -> CavalRe
    
    Clean exports
    
    Construct all currencies
    
    Add MIT license

--- src/Positions.jl
+++ src/Positions.jl
@@ -1,21 +1,52 @@
 module Positions
 
-using Reexport
-@reexport using FinancialInstruments
+using FinancialInstruments, FixedPointDecimals
+
+export Currencies, Currency
+export Countries, Country
+export FinancialInstruments, FinancialInstrument, Cash
 export Position
 
-struct Position{P<:Union{Currency,FinancialInstrument}}
-    amount::Float64
+abstract type AbstractPosition end
+
+struct Position{F<:FinancialInstrument,A<:Real} <: AbstractPosition
+    amount::A
 end
-Position(p::P,a::Float64) where P<:Union{Currency,FinancialInstrument} = Position{typeof(p)}(a)
+Position(p,a) = Position{typeof(p),typeof(a)}(a)
+Position(c::Cash{C},a) where C = Position{Cash{C},FixedDecimal{Int,Currencies.unit(C())}}(FixedDecimal{Int,Currencies.unit(C())}(a))
 
-Base.:+(p1::Position{P},p2::Position{P}) where P<:Union{Currency,FinancialInstrument} = Position{P}(p1.amount+p2.amount)
-Base.:-(p1::Position{P},p2::Position{P}) where P<:Union{Currency,FinancialInstrument} = Position{P}(p1.amount-p2.amount)
+Base.promote_rule(::Type{Position{F,A1}}, ::Type{Position{F,A2}}) where {F,A1,A2} = Position{F,promote_type(A1,A2)}
+Base.convert(::Type{Position{F,A}}, x::Position{F,A}) where {F,A} = x
+Base.convert(::Type{Position{F,A}}, x::Position{F,<:Real}) where {F,A} = Position{F,A}(convert(A,x.amount))
 
-Base.:/(p1::Position{P},p2::Position{P}) where P<:Union{Currency,FinancialInstrument} = p1.amount/p2.amount
-Base.:/(p::Position{P},k::R) where P<:Union{Currency,FinancialInstrument} where R<:Real = Position{P}(c.amount/k)
+Base.:+(p1::Position{F,A},p2::Position{F,A}) where {F,A} = Position{F,A}(p1.amount+p2.amount)
+Base.:+(p1::AbstractPosition,p2::AbstractPosition) = +(promote(p1,p2)...)
 
-Base.:*(p::Position{P},k::R) where P<:Union{Currency,FinancialInstrument} where R<:Real = Position{P}(p.amount*k)
-Base.:*(k::R,p::Position{P}) where P<:Union{Currency,FinancialInstrument} where R<:Real = p*k
+Base.:-(p1::Position{F,A},p2::Position{F,A}) where {F,A} = Position{F,A}(p1.amount-p2.amount)
+Base.:-(p1::AbstractPosition,p2::AbstractPosition) = -(promote(p1,p2)...)
+
+Base.:/(p1::Position{F,A},p2::Position{F,A}) where {F,A} = p1.amount/p2.amount
+Base.:/(p1::AbstractPosition,p2::AbstractPosition) = /(promote(p1,p2)...)
+
+# TODO: Should scalar multiplication and division respect the Position type or do normal promotion?
+
+# Base.:/(p::Position{F,A},k::R) where {F,A,R<:Real} = Position{F,promote_type(A,R)}(p.amount/k)
+Base.:/(p::Position{F,A},k::R) where {F,A,R<:Real} = Position{F,A}(p.amount/k)
+
+# Base.:*(k::R,p::Position{F,A}) where {F,A,R<:Real} = Position{F,promote_type(A,R)}(p.amount*k)
+Base.:*(k::R,p::Position{F,A}) where {F,A,R<:Real} = Position{F,A}(p.amount*k)
+Base.:*(p::Position{F,A},k::R) where {F,A,R<:Real} = k*p
+
+Base.show(io::IO,c::Position{Cash{C}}) where C = print(io,c.amount," ",C())
+Base.show(io::IO,::MIME"text/plain",c::Position{Cash{C}}) where C = print(io,c.amount," ",C())
+
+Base.zero(::Type{Position{F,A}}) where {F,A} = Position{F,A}(zero(A))
+Base.one(::Type{Position{F,A}}) where {F,A} = Position{F,A}(one(A))
+
+for (sym,ccy) in Currencies.list
+    @eval Positions begin
+        $sym = Position(Cash($ccy),1)
+    end
+end
 
 end # module
