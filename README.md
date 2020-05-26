854153e6b470ae6d4bff1b93ccdfcc6d923710c9
ScottPJones <scottjones@alum.mit.edu>
2020-05-26 10:46:09 -0400

    Add unit tests and documentation

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # Positions.jl
+
+[![Build Status](https://travis-ci.org/JuliaFinance/Positions.jl.svg?branch=master)](https://travis-ci.org/JuliaFinance/Positions.jl)
+[![Build status](https://ci.appveyor.com/api/projects/status/chnj7xc6r0deux92/branch/master?svg=true)](https://ci.appveyor.com/project/EricForgy/currencies-jl/branch/master)
+
+This is a base package for the JuliaFinance ecosytem.
+
+It provides a singleton type `Currency` based on standard ISO 4167 currency codes to be used primarily for dispatch in other JuliaFinance packages together with three methods:
+
+- `name`: The name of the currency.
+- `code`: The ISO 4167 code for the currency.
+- `unit`: The minor unit, i.e. number of decimal places, for the currency.
+
+(this functionality used to be from the Currencies.jl package)
+
+These are simple labels, such as `Currency{:USD}`, `Currency{:EUR}`.
+
+In addition, this package provides an abstract type `FinancialInstrument`.
+
+A financial instrument is a tradeable monetary contract that creates an asset for some parties while, at same time, creating a liability for others.
+
+Examples of financial instruments include stocks, bonds, loans, derivatives, etc. However, the most basic financial instruments are currencies.
+
+When a currency is thought of as a financial instrument (as opposed to a mere label used in UI component), we choose to refer to it as `Cash` (as it would appear in a balance sheet).
+The `Cash` type keeps track of the number of minor units as part of the type, for performance and dispatching reasons.
+
+Short constants are set up, matching the ISO 4167 names, so that you can use
+`USD` instead of `Cash{Currency{:USD},2}()`
+
+Finally, this package also provides an `AbstractPosition` type, as well as a `Position` concrete type.
+`Position` represents ownership of a financial instrument including the quantity of that financial instrument. For example, Microsoft stock (MSFT) is a financial instrument. A position could be 1,000 shares of MSFT.
+
+In the case of currency, `Positions.USD` would be a financial instrument and owning $1,000 would mean you own 1,000 units of the financial instrument `Positions.USD`.
+
+If you are building a financial application that requires adding, subtracting, multiplying and dividing currencies, then you want to use `Positions`.
+
+For example:
+```julia
+julia> using Positions
+
+julia> using Positions: USD, JPY
+
+julia> 10USD
+10.00USD
+
+julia> 10USD+20USD
+30.00USD
+
+julia> 5*20USD
+100.00USD
+
+julia> 100USD/5
+20.00USD
+
+julia> 100USD/5USD
+FixedDecimal{Int64,2}(20.00)
+
+julia> 100JPY/5JPY
+FixedDecimal{Int64,0}(20)
+
+julia> 100USD+100JPY
+ERROR: promotion of types Position{Cash{Currency{:USD}},FixedPointDecimals.FixedDecimal{Int64,2}} and Position{Cash{Currency{:JPY}},FixedPointDecimals.FixedDecimal{Int64,0}} failed to change any arguments
+```
+
+Note that algebraic operations of currency positions require the positions to be of the same financial instrument. In this case, they must be the same currency as indicated by the error in the last command above.
+
+See also:
+
+- [Markets.jl](https://github.com/JuliaFinance/Markets.jl)
+- [GeneralLedgers.jl](https://github.com/JuliaFinance/GeneralLedgers.jl)
+
+## Data Source
+
+Data for this package was obtained from https://datahub.io/core/country-codes.
+
--- src/Positions.jl
+++ src/Positions.jl
@@ -1,7 +1,12 @@
 """
 Positions package
 
-... fill in documentation here ...
+This provides the `Currency` singleton type, based on the ISO 4167 standard codes,
+as well as the `FinancialInstrument` and `AbstractPosition` abstract types,
+and the `Cash` and `Position` parameterized types, for dealing with all sorts of
+currencies and other financial instruments in an easy fashion.
+
+See README.md for the full documentation
 
 Copyright 2019-2020, Eric Forgy, Scott P. Jones, and other contributors
 
@@ -12,11 +17,21 @@ module Positions
 using FixedPointDecimals
 
 export Position, Currency, Currencies, Cash, AbstractPosition, FinancialInstrument
+export currency, cash, unit, code, name
 
+"""
+This is an abstract type, for dealing generically with positions of financial instruments
+"""
 abstract type AbstractPosition end
 
+"""
+This is an abstract type for `FinancialInstruments` such as `Cash`, `Stock`,
+"""
 abstract type FinancialInstrument end
 
+"""
+This is a singleton type, intended to be used as a label for dispatch purposes
+"""
 struct Currency{Symbol} end
 
 # TODO: This should be an inner constructor
@@ -24,6 +39,26 @@ Currency{T}() where {T<:Symbol} =
     haskey(Currencies, T) ? Currencies[T] : (Currencies[T] = new())
 
 include(joinpath(dirname(pathof(@__MODULE__)), "..", "deps", "currencies.jl"))
+
+"""
+Returns the currency type associated with this value or type
+"""
+function currency end
+
+"""
+Returns the minor unit associated with this value or type
+"""
+function unit end
+
+"""
+Returns the ISO 4167 code associated with this value or type
+"""
+function code end
+
+"""
+Returns the ISO 4167 name associated with this value or type
+"""
+function name end
 
 currency(::Type{Currency{T}}) where {T} = T
 unit(::Type{Currency{T}}) where {T} = Currencies[T][2]
@@ -35,6 +70,10 @@ unit(c::Currency) = unit(typeof(c))
 code(c::Currency) = code(typeof(c))
 name(c::Currency) = name(typeof(c))
 
+"""
+`Cash` is a `FinancialInstrument`, that represents a particular currency as well
+as the number of digits in the minor units, typically 0, 2, or 3
+"""
 struct Cash{C<:Currency, N} <: FinancialInstrument end
 Cash(::C) where {C<:Currency} = Cash{C,unit(C)}()
 Cash(c::Symbol) = Cash(Currency{c}())
@@ -43,13 +82,19 @@ currency(::Type{Cash{C}}) where {C} = C
 currency(::Cash{C}) where {C} = C
 unit(::Type{Cash{C,N}}) where {C,N} = N
 unit(::Cash{C,N}) where {C,N} = N
+code(::Cash{C}) where {C} = code(C)
+name(::Cash{C}) where {C} = name(C)
 
 Base.show(io::IO, ::Cash{<:Currency{T}}) where {T} = print(io, string(T))
 
+# Set up short names for all of the currencies (as instances of the Cash FinancialInstruments)
 for (sym, ccy) in Currencies
     @eval $sym = Cash($(ccy[1]()))
 end
 
+"""
+Position represents a certain quantity of a particular financial instrument
+"""
 struct Position{F<:FinancialInstrument,A<:Real} <: AbstractPosition
     amount::A
     function Position(::Type{FI}, a) where {C,N,FI<:Cash{C,N}}
@@ -61,6 +106,9 @@ end
 Position(::F, a) where {F<:FinancialInstrument} = Position(F, val)
 Position{F}(val) where {F<:FinancialInstrument} = Position(F, val)
 
+"""
+Returns the Cash financial instrument (as an instance) for a position
+"""
 cash(::Position{F}) where {F} = F()
 
 Base.promote_rule(::Type{Position{F,A1}}, ::Type{Position{F,A2}}) where {F,A1,A2} =
@@ -70,16 +118,16 @@ Base.convert(::Type{Position{F,A}}, x::Position{F,<:Real}) where {F,A} =
     Position{F,A}(convert(A, x.amount))
 
 Base.:+(p1::Position{F1}, p2::Position{F2}) where {F1,F2} =
-    error("Can't add Positions of different FinancialInstruments $F1, $F2")
+    error("Can't add Positions of different FinancialInstruments $(F1()), $(F2())")
 Base.:+(p1::Position{F}, p2::Position{F}) where {F} = Position{F}(p1.amount + p2.amount)
 Base.:+(p1::AbstractPosition, p2::AbstractPosition) = +(promote(p1, p2)...)
 
 Base.:-(p1::Position{F1}, p2::Position{F2}) where {F1,F2} =
-    error("Can't subtract Positions of different FinancialInstruments $F1, $F2")
+    error("Can't subtract Positions of different FinancialInstruments $(F1()), $(F2())")
 Base.:-(p1::Position{F}, p2::Position{F}) where {F} = Position{F}(p1.amount - p2.amount)
 Base.:-(p1::AbstractPosition, p2::AbstractPosition) = -(promote(p1, p2)...)
 
-Base.:/(p1::Position{F}, p2::Position{F}) where {F} = p1.amount / p2.amount
+Base.:/(p1::Position, p2::Position) = p1.amount / p2.amount
 Base.:/(p1::AbstractPosition, p2::AbstractPosition) = /(promote(p1, p2)...)
 
 Base.:/(p::Position{F}, k::Real) where {F} = Position{F}(p.amount / k)
